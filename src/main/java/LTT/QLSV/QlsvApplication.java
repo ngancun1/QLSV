c1f9119d8311c7d39315d0b4da78cdf2ae2a8621
Anh Dũng Lê <dungla@lifetimetech.vn>
2018-08-20 09:54:55 +0700

    lul

---
+++ src/main/java/LTT/QLSV/QlsvApplication.java
@@ -0,0 +1,12 @@
+package LTT.QLSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlsvApplication.class, args);
+	}
+}
